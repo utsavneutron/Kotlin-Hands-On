f210826e9cbba3d3010d56545a01fac1490c7173
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2022-03-19 10:39:16 -0500

    added datatypes

---
+++ datatypes.kt
@@ -0,0 +1,16 @@
+// println(""" 1
+//            *  11
+//            * 111
+//           * 1111
+//           """.trimMargin()) 
+
+fun main(Array<String>) {
+    val myInt = 10
+    var myLong = 22L
+
+    myLong = myInt.toLong()
+
+    val myByte: Byte = 111
+    var myShort: Short
+    myShort = myByte.toShort()
+}
